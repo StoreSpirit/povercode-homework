f96e069f0dd7d05b2bc612ab61a87e41196a2b0d
StoreSpirit <96310014+StoreSpirit@users.noreply.github.com>
2023-02-22 18:21:50 +0200

    Add callback
    Add active modal window with callback function

--- module_5/callback window/index.js
+++ module_5/callback window/index.js
@@ -1,1 +1,26 @@
-'use strict'
+'use strict';
+
+const modalOpen = document.getElementById('modal-open');
+const modalClose = document.getElementById('modal-close');
+const modal = document.getElementById('modal_callback');
+const form_1 = document.getElementById('callback_form')
+const name_1 = document.getElementById('name_inp')
+const email_1 = document.getElementById('email_inp')
+const tel_1 = document.getElementById('tel_inp')
+
+function openModal() {    
+    modal.classList.add('opened');
+}
+
+function closeModal() {
+    modal.classList.remove('opened');
+}
+
+modalClose.onclick = closeModal;
+
+modalOpen.addEventListener('click', openModal);
+document.addEventListener('click', function(event){
+    if (event.target !== modal && event.target !== modalOpen && event.target !== form_1 && event.target !== name_1 && event.target !== email_1 && event.target !== tel_1){
+        closeModal()
+    }
+})
