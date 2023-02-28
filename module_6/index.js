df7766609a3edfc4af99d7e85cffacfc4c78c77e
StoreSpirit <96310014+StoreSpirit@users.noreply.github.com>
2023-02-28 15:09:39 +0200

    Add mock
    Added html code, img and js logic for mock-data

--- module_6/index.js
+++ module_6/index.js
@@ -23,3 +23,46 @@ document.addEventListener('click', function(event){
     }
 })
 
+const clocksArr = [{   
+    img: './img/elit_clock.png',
+    name: 'Elit clock',
+    cost: '250$'
+},
+{
+    img: './img/gold_clock.png',
+    name: 'Gold clock',
+    cost: '300$'    
+},
+{
+    img: './img/hi_tech_clock.png',
+    name: 'Hi-Tech clok',
+    cost: '150$'    
+},
+/* {
+    img: './img/coffee_clock.png',
+    name: 'Coffee clock',
+    cost: '75$'    
+},
+{
+    img: './img/old_clock.png',
+    name: 'Old clock',
+    cost: '500$'    
+},
+{
+    img: './img/smart_clock.png',
+    name: 'Smart clock',
+    cost: '50$'    
+}*/]
+
+const clocks = document.getElementById('clocks-list')
+
+function getHtmlFromClock (clock) {
+    return `<li><img src="${clock.img}" alt="${clock.name}"><p class="name_clock">${clock.name}</p><p>Price:${clock.cost}</p></li>`;
+}
+let html = '';
+
+for (let i = 0; i < clocksArr.length; i++) {
+    html += getHtmlFromClock (clocksArr[i]);
+}
+
+clocks.innerHTML = html;
